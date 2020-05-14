3e4cf7f646eb2c704a7f3fcb9e2c183e09e5aaf3
tina.liu <tina.liu@inmobi.com>
2020-05-14 15:56:39 +0800

    V3:tableview重写

---
+++ LTAdShow/LTSUATableViewController.h
@@ -0,0 +1,17 @@
+//
+//  LTSUATableViewController.h
+//  LTAdShow
+//
+//  Created by tina.liu on 2020/5/14.
+//  Copyright © 2020 tina.liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LTSUATableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
